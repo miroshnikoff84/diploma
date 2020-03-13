f5e8f3d41860cf8566623266cbf23f54c9e44866
miroshnikoff84 <miroshnikoff84@mail.ru>
2020-03-13 18:09:33 +0300

    upd js

--- src/script.js
+++ src/script.js
@@ -3,10 +3,8 @@
 const modalWindow = () => {
   const btnAlertModal = (btn, popupWindow, popupWindowContent, form) => {
 
-    let count = 0;
     btn.forEach((elem) => {
       elem.addEventListener('click', (event) => {
-        event.preventDefault();
         if (form) {
           for (const elem of form.elements) {
             if (elem.tagName.toLowerCase() !== 'button' && elem.type !== 'button') {
@@ -29,7 +27,6 @@ const modalWindow = () => {
 
       const countPopupNone = () => {
         popupWindow.style.display = 'none';
-        count = 0;
       };
       let target = event.target;
       if (target.matches('.popup-close')) {
@@ -42,7 +39,7 @@ const modalWindow = () => {
       }
     });
   };
-  
+  //Popup-call
   const popupCall = () => {
     const popupCall = document.querySelector('.popup-call'),
       callBtn = document.querySelectorAll('.call-btn'),
@@ -52,6 +49,100 @@ const modalWindow = () => {
     btnAlertModal(callBtn, popupCall, popupContentCall, callForm);
   };
   popupCall();
+
+  
+
 };
 
 modalWindow(); 
+
+const addBlocks = () => {
+  const shadowBlocks = document.querySelectorAll(`.col-xs-12.col-sm-6.col-md-4`),
+    btnAddSentence = document.querySelector('.add-sentence-btn');
+
+
+  btnAddSentence.addEventListener('click', (event) => {
+    event.preventDefault();
+    shadowBlocks.forEach((elem) => {
+      if (elem.matches('.visible-sm-block') || elem.matches('.hidden')) {
+        elem.classList.remove('visible-sm-block', 'hidden');
+        btnAddSentence.style.cssText = `transform: scale(0)`;
+      }
+
+    });
+
+  });
+
+};
+
+addBlocks();
+
+const accordion = () => {
+  const btnAccordion = document.querySelectorAll(`a[data-parent="#accordion"]`),
+    collapseOneId = document.getElementById('collapseOne'),
+    collapseTwoId = document.getElementById('collapseTwo'),
+    collapseThreeId = document.getElementById('collapseThree');
+  let collapseFourId = document.getElementById('collapseFour');
+
+  btnAccordion.forEach((elem) => {
+    elem.addEventListener('click', (event) => {
+      event.preventDefault();
+      //One
+      if (elem.closest('#headingOne')) {
+        if (collapseOneId.style.display === 'none') {
+          collapseOneId.style.display = 'block';
+          collapseTwoId.style.display = 'none';
+          collapseThreeId.style.display = 'none';
+          collapseFourId.style.display = 'none';
+
+        } else {
+          collapseOneId.style.display = 'block';
+        }
+      }
+      //Two
+      if (elem.closest('#headingTwo') || elem.matches('.btnOne')) {
+
+        if (collapseTwoId.style.display === 'block') {
+          collapseTwoId.style.display = 'block';
+
+        } else {
+          collapseOneId.style.display = 'none';
+          collapseTwoId.style.display = 'block';
+          collapseThreeId.style.display = 'none';
+          collapseFourId.style.display = 'none';
+        }
+      }
+      //Three
+      if (elem.closest('#headingThree') || elem.matches('.btnTwo')) {
+
+        if (collapseThreeId.style.display === 'block') {
+          collapseThreeId.style.display = 'block';
+
+        } else {
+          collapseOneId.style.display = 'none';
+          collapseTwoId.style.display = 'none';
+          collapseThreeId.style.display = 'block';
+          collapseFourId.style.display = 'none';
+        }
+      }
+      //Four
+      if (elem.closest('#headingFour') || elem.matches('.btnThree')) {
+
+        if (collapseFourId.style.display === 'block') {
+          collapseFourId.style.display = 'block';
+
+        } else {
+          collapseOneId.style.display = 'none';
+          collapseTwoId.style.display = 'none';
+          collapseThreeId.style.display = 'none';
+          collapseFourId.style.display = 'block';
+        }
+      }
+
+    });
+
+  });
+
+};
+
+accordion();
