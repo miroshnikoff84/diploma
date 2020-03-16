5b87d118bb0b03c9aba993d353125e07cd983347
miroshnikoff84 <miroshnikoff84@mail.ru>
2020-03-16 05:48:34 +0300

    fix acc

--- src/modules/test.js
+++ src/modules/test.js
@@ -1,112 +1,149 @@
-// 'use strict';
+'use strict';
 
 // const accordionTwo = () => {
-//   const panelCollapseOne = document.querySelector('#collapseOne-two'),
+
+//   const btn = document.querySelectorAll(`a[data-parent="#accordion-two"]`);
+//     const panelCollapseOne = document.querySelector('#collapseOne-two'),
 //         panelCollapseTwo = document.querySelector('#collapseTwo-two'),
-//         panelCollapseThree = document.querySelector('#collapseThree-two');
-  
-//   const btn = document.querySelectorAll('.panel-heading'),
-//     linkText = document.querySelectorAll(`a[data-toggle="collapse"]`),
-//     inCollapse = document.querySelectorAll('.collapse');
+//     panelCollapseThree = document.querySelector('#collapseThree-two');
+//   console.log(btn);
 
-//   for (let i = 0; i < btn.length; i++){
-//     btn[i].style.cursor = 'pointer';
-//   }
 
-//   // const animAcc = (panel) => {
-    
-//   //     let start = Date.now();
+//   const slideAdd = (panelCollapse, collapseHeight) => { //плавное появление
+//     let count = 0;
+//     let panelInterval;
+//     const panelOpacityTwo = function () {
+//       panelInterval = requestAnimationFrame(panelOpacityTwo);
+//       count = count + 3;
+//       if (count <= collapseHeight) { 
 
-//   //     let timer = setInterval( ()=> {
-//   //       let timePassed = Date.now() - start;
+//         panelCollapse.style.maxHeight = count + 'px';
+//       } else {
+//         cancelAnimationFrame(panelInterval);
+//       }
+//     };
+//     panelOpacityTwo();
+//   };
+//   const slideKill = (panelCollapse, collapseHeight) => { //плавное появление
+//     let count = collapseHeight;
+//     let panelInterval;
+//     const panelOpacityTwoo = function () {
+//       panelInterval = requestAnimationFrame(panelOpacityTwoo);
+//       count = count - 3;
+//       if (count <= collapseHeight) { 
 
-//   //       panel.style.height = timePassed / 5 + 'px';
-
-//   //       if (timePassed > 2000) {
-//   //         clearInterval(timer);
-//   //       } 
-
-//   //     }, 20);
-//   // };
+//         panelCollapse.style.maxHeight = count + 'px';
+//       } else {
+//         cancelAnimationFrame(panelInterval);
+//       }
+//     };
+//     panelOpacityTwoo();
+//   };
 
 //   btn.forEach((elem) => {
 //     elem.addEventListener('click', (event) => {
 //       event.preventDefault();
 //       let target = event.target;
-//       inCollapse[4].style.overflow = 'hidden';
-//       let height = 0;
 
-//       if (target === (btn[4]) || target === (linkText[7])) {
-//         let timer = setInterval(() => {
-//           let maxHeight = 136;
-            
-          
-//           inCollapse[4].style.height = height + 'px';
-//           //     inCollapse[4].style.cssText = `height: ${height}px;
-//           // overflow: hidden;
-//           // `;
-            
-//             console.log(height);
-          
-            
-//           if (height > maxHeight) {
-//             clearInterval(timer);
-//         }
-          
-//         }, 24);
-        
-        
       
+
+//         if (target === (btn[0])) {
+//           panelCollapseOne.style.display = 'block';
+//           slideAdd(panelCollapseOne, 138);
+//         slideKill(panelCollapseTwo, 138);
+//         slideKill(panelCollapseThree,  138);
+//       } else if (target === (btn[1])) {
+//         panelCollapseTwo.style.display = 'block';
+//         slideAdd(panelCollapseTwo, 138);
+//         slideKill(panelCollapseOne, 138);
+//         slideKill(panelCollapseThree, 138);
+//       } else if (target === (btn[2])) {
+//         panelCollapseThree.style.display = 'block';
+//         slideAdd(panelCollapseThree, 138);
+//         slideKill(panelCollapseOne, 138);
+//         slideKill(panelCollapseTwo, 138);
 //       }
-        
       
 //     });
+
 //   });
 
-
-
-  
-//   console.log(btn);
-//   console.log(linkText);
-//   console.log(inCollapse);
-//   // btn.forEach((elem) => {
-//   //   elem.addEventListener('click', (event) => {
-//   //     event.preventDefault();
-//   //     let target = event.target;
-
-//   //     if (target === (btn[0])) {
-//   //       panelCollapseOne.style.display = 'block';
-//   //       panelCollapseTwo.style.display = 'none';
-//   //       panelCollapseThree.style.display = 'none';
-//   //     } else if (target === (btn[1])) {
-//   //       panelCollapseOne.style.display = 'none';
-//   //       panelCollapseTwo.style.display = 'block';
-//   //       panelCollapseThree.style.display = 'none';
-//   //     } else if (target === (btn[2])) {
-//   //       panelCollapseOne.style.display = 'none';
-//   //       panelCollapseTwo.style.display = 'none';
-//   //       panelCollapseThree.style.display = 'block';
-//   //     }
-//   //   });
-//   // });
 // };
 
 // accordionTwo();
 
-// // const btn = document.querySelectorAll(`a[data-parent="#accordion-two"]`);
-// // var disc = document.querySelectorAll('.test_disc');
+const accordionTwo = () => {
 
-// // for (var i = 0; i < btn_title.length; i++) {
-// //   btn[i].addEventListener('click', fun_open);
+  const btn = document.querySelectorAll(`a[data-parent="#accordion-two"]`);
+    const panelCollapseOne = document.querySelector('#collapseOne-two'),
+        panelCollapseTwo = document.querySelector('#collapseTwo-two'),
+    panelCollapseThree = document.querySelector('#collapseThree-two');
 
-// //   function fun_open(event) {
-// //     for (var i = 0; i < btn_title.length; i++) {
-// //       disc[i].classList.remove('test_disc_show');
-// //       if (btn_title[i] == event.currentTarget) {
-// //         disc[i].classList.toggle('test_disc_show');
-// //       }
-// //     }
+  const slideAdd = (panelCollapse, collapseHeight) => { //плавное появление
+    let count = 0;
+    let panelInterval;
+    let curentHeight;
+    const panelOpacityTwo = function () {
+      panelInterval = requestAnimationFrame(panelOpacityTwo);
+      curentHeight = count;
+      count = count + 5;
+      if (count <= collapseHeight) { 
 
-// //   }
+        panelCollapse.style.maxHeight = count + 'px';
+      } else {
+        cancelAnimationFrame(panelInterval);
+      }
+    };
+    panelOpacityTwo();
+  };
+  const slideKill = (panelCollapse, collapseHeightDown) => { //плавное убывание
+    let count = collapseHeightDown;
+    let panelInterval;
+    const panelOpacityTwoo = function () {
+      panelInterval = requestAnimationFrame(panelOpacityTwoo);
+      count = count - 5;
+      if (count <= collapseHeightDown) { 
 
-// // }
+        panelCollapse.style.maxHeight = count + 'px';
+      } else {
+        cancelAnimationFrame(panelInterval);
+      }
+    };
+    panelOpacityTwoo();
+  };
+
+  btn.forEach((elem) => {
+    elem.addEventListener('click', (event) => {
+      event.preventDefault();
+      let target = event.target;
+      panelCollapseTwo.style.maxHeight = 0 + 'px';
+      panelCollapseThree.style.maxHeight = 0 + 'px';
+
+      
+
+      if (target === (btn[0]) && panelCollapseOne.style.maxHeight === 0 + 'px') {
+          panelCollapseOne.style.display = 'block';
+        slideAdd(panelCollapseOne, 140);
+        slideKill(panelCollapseTwo, 140);
+          slideKill(panelCollapseThree, 140);
+      } else if (target === (btn[1]) && panelCollapseTwo.style.maxHeight === 0 + 'px') {
+        panelCollapseTwo.style.display = 'block';
+        slideAdd(panelCollapseTwo, 140);
+        panelCollapseOne.style.maxHeight = 0 + 'px';
+        slideKill(panelCollapseOne, 140);
+          slideKill(panelCollapseThree, 140);
+      } else if (target === (btn[2]) && panelCollapseThree.style.maxHeight === 0 + 'px') {
+        panelCollapseThree.style.display = 'block';
+        slideAdd(panelCollapseThree, 140);
+        panelCollapseOne.style.maxHeight = 0 + 'px';
+        slideKill(panelCollapseOne, 140);
+          slideKill(panelCollapseTwo, 140);
+      }
+      
+    });
+
+  });
+
+};
+
+accordionTwo();
--- src/script.js
+++ src/script.js
@@ -108,31 +108,77 @@ addBlocks();
 
 //accordionTwo
 const accordionTwo = () => {
-  const panelCollapseOne = document.querySelector('#collapseOne-two'),
-    panelCollapseTwo = document.querySelector('#collapseTwo-two'),
-    panelCollapseThree = document.querySelector('#collapseThree-two');
 
   const btn = document.querySelectorAll(`a[data-parent="#accordion-two"]`);
+    const panelCollapseOne = document.querySelector('#collapseOne-two'),
+        panelCollapseTwo = document.querySelector('#collapseTwo-two'),
+    panelCollapseThree = document.querySelector('#collapseThree-two');
+
+  const slideAdd = (panelCollapse, collapseHeight) => { //плавное появление
+    let count = 0;
+    let panelInterval;
+    let curentHeight;
+    const panelOpacityTwo = function () {
+      panelInterval = requestAnimationFrame(panelOpacityTwo);
+      curentHeight = count;
+      count = count + 5;
+      if (count <= collapseHeight) { 
+
+        panelCollapse.style.maxHeight = count + 'px';
+      } else {
+        cancelAnimationFrame(panelInterval);
+      }
+    };
+    panelOpacityTwo();
+  };
+  const slideKill = (panelCollapse, collapseHeightDown) => { //плавное убывание
+    let count = collapseHeightDown;
+    let panelInterval;
+    const panelOpacityTwoo = function () {
+      panelInterval = requestAnimationFrame(panelOpacityTwoo);
+      count = count - 5;
+      if (count <= collapseHeightDown) { 
+
+        panelCollapse.style.maxHeight = count + 'px';
+      } else {
+        cancelAnimationFrame(panelInterval);
+      }
+    };
+    panelOpacityTwoo();
+  };
+
   btn.forEach((elem) => {
     elem.addEventListener('click', (event) => {
       event.preventDefault();
       let target = event.target;
+      panelCollapseTwo.style.maxHeight = 0 + 'px';
+      panelCollapseThree.style.maxHeight = 0 + 'px';
 
-      if (target === (btn[0])) {
-        panelCollapseOne.style.display = 'block';
-        panelCollapseTwo.style.display = 'none';
-        panelCollapseThree.style.display = 'none';
-      } else if (target === (btn[1])) {
-        panelCollapseOne.style.display = 'none';
+      
+
+      if (target === (btn[0]) && panelCollapseOne.style.maxHeight === 0 + 'px') {
+          panelCollapseOne.style.display = 'block';
+        slideAdd(panelCollapseOne, 140);
+        slideKill(panelCollapseTwo, 140);
+          slideKill(panelCollapseThree, 140);
+      } else if (target === (btn[1]) && panelCollapseTwo.style.maxHeight === 0 + 'px') {
         panelCollapseTwo.style.display = 'block';
-        panelCollapseThree.style.display = 'none';
-      } else if (target === (btn[2])) {
-        panelCollapseOne.style.display = 'none';
-        panelCollapseTwo.style.display = 'none';
+        slideAdd(panelCollapseTwo, 140);
+        panelCollapseOne.style.maxHeight = 0 + 'px';
+        slideKill(panelCollapseOne, 140);
+          slideKill(panelCollapseThree, 140);
+      } else if (target === (btn[2]) && panelCollapseThree.style.maxHeight === 0 + 'px') {
         panelCollapseThree.style.display = 'block';
+        slideAdd(panelCollapseThree, 140);
+        panelCollapseOne.style.maxHeight = 0 + 'px';
+        slideKill(panelCollapseOne, 140);
+          slideKill(panelCollapseTwo, 140);
       }
+      
     });
+
   });
+
 };
 
 accordionTwo();
